f12896e3d9066d5dcad6daf8e931985a360db872
uw-ray <uw.rayblaga90@gmail.com>
2014-06-18 15:00:54 -0400

    work, work

--- vidquiz/vidquiz/vidquiz.py
+++ vidquiz/vidquiz/vidquiz.py
@@ -306,7 +306,7 @@ class VideoQuiz(XBlock):
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/vidquiz.css"))
         frag.add_javascript(self.resource_string("static/js/src/vidquiz.js"))
-        frag.add_javascript(self.resource_string("static/js/src/popcorn-complete.min.js"))
+        frag.add_javascript(self.resource_string("static/js/src/popcorn-complete.js"))
         #frag.add_javascript(self.resource_string("static/js/src/jquery-1.11.1.min.js"))
 
 
