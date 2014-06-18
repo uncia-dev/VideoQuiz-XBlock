992e8718b49e7fe56c64a4c586aba25d4208a4f8
uw-ray <uw.rayblaga90@gmail.com>
2014-06-18 11:46:13 -0400

    test

--- vidquiz/vidquiz/vidquiz.py
+++ vidquiz/vidquiz/vidquiz.py
@@ -313,7 +313,7 @@ class VideoQuiz(XBlock):
         html = self.resource_string("static/html/vidquiz.html")
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/vidquiz.css"))
-        frag.add_javascript(self.resource_string("static/js/src/jquery-1.11.1.min.js"))
+        #frag.add_javascript(self.resource_string("static/js/src/jquery-1.11.1.min.js"))
         frag.add_javascript(self.resource_string("static/js/src/popcorn-complete.min.js"))
         frag.add_javascript(self.resource_string("static/js/src/vidquiz.js"))
         frag.initialize_js('VideoQuiz')
@@ -334,7 +334,7 @@ class VideoQuiz(XBlock):
         html = self.resource_string("static/html/vidquiz_studio.html")
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/vidquiz.css"))
-        frag.add_javascript(self.resource_string("static/js/src/jquery-1.11.1.min.js"))
+        #frag.add_javascript(self.resource_string("static/js/src/jquery-1.11.1.min.js"))
         frag.add_javascript(self.resource_string("static/js/src/vidquiz_studio.js"))
         frag.initialize_js('VideoQuizStudio')
 
