06973492b7ddb86df57eabc6ec24da05ecba291b
Raymond Blaga <uw.rayblaga90@gmail.com>
2014-08-11 13:37:50 -0400

    Update vidquiz_studio.js

--- vidquiz/static/js/vidquiz_studio.js
+++ vidquiz/static/js/vidquiz_studio.js
@@ -1,10 +1,10 @@
-/* Javascript for VideoQuiz. */
+/* Javascript for VideoQuiz, Studio page. */
 function VideoQuizStudio(runtime, element) {
 
     /* Update fields of the form to the current values */
     function formUpdate(data) {
 
-        $(".title").val(data.title);
+        $(".vid_title").val(data.title);
         $(".quiz_content").val(data.quiz_content);
         $(".href").val(data.href);
         $(".width").val(data.width);
@@ -30,7 +30,7 @@ function VideoQuizStudio(runtime, element) {
                 type: "POST",
                 url: runtime.handlerUrl(element, 'studio_submit'),
                 data: JSON.stringify({
-                    "title": $('.title').val(),
+                    "title": $('.vid_title').val(),
                     "quiz_content": $('.quiz_content').val(),
                     "href": $('.href').val(),
                     "width": $('.width').val(),
