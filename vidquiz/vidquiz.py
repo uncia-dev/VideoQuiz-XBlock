e9e3c060d1dcdb4ec7a5085b04d0fddf314cfbf4
uw-ray <uw.rayblaga90@gmail.com>
2014-08-13 14:09:50 -0400

    testing

--- vidquiz/vidquiz.py
+++ vidquiz/vidquiz.py
@@ -113,7 +113,9 @@ class VideoQuiz(XBlock):
         print("Loading quiz ...")
 
         # grab questions, answers, etc from form
-        for line in self.quiz_content.split('~~'):
+        for line in self.quiz_content.split('\n'):
+
+            print(repr(line))
 
             '''
             each line will contain the following:
